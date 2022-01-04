36c9c0d2baddd4c1ce52b9c29f2d79e1769a77f5
venoom <venoomtulip@gmail.com>
2022-01-04 16:23:16 +0700

    add arch power

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4177,6 +4177,18 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+static unsigned int Lgentle_fair_sleepers = 0;
+static unsigned int Larch_power = 1;
+
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
+void relay_ap(unsigned int ap)
+{
+	Larch_power = ap;
+}
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -13105,3 +13117,4 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 }
 
 #endif /* CONFIG_SCHED_WALT */
+
